b22009f446d293df540a89142e0fd15fc2f9b059
Sharda Borse <shardaborse1@gmail.com>
2021-02-20 20:58:18 +0530

    152 ms

--- solutions/975. Odd Even Jump.java
+++ solutions/975. Odd Even Jump.java
@@ -1,25 +1,24 @@
 class Solution {
     public int oddEvenJumps(int[] A) {
-        
         int n=A.length;
-        TreeMap<Integer,Integer> map=new TreeMap();
-        boolean higher[]=new boolean[n];
         boolean lower[]=new boolean[n];
-        lower[n-1]=higher[n-1]=true;
-        
-        int res=1;
+        boolean higher[]=new boolean[n];
+        lower[n-1]=true;
+        higher[n-1]=true;
+        TreeMap<Integer,Integer> map=new TreeMap();
         map.put(A[n-1],n-1);
+        int count=1;
+        
         for(int i=n-2;i>=0;i--){
-            Map.Entry<Integer,Integer> hi=map.ceilingEntry(A[i]),lo=map.floorEntry(A[i]);
-            if(hi!=null)
-                higher[i]=lower[(int)hi.getValue()];
-            if(lo!=null)
-                lower[i]=higher[(int)lo.getValue()];
+            Map.Entry<Integer,Integer> high=map.ceilingEntry(A[i]),low=map.floorEntry(A[i]);
+            if(high!=null)
+                higher[i]=lower[high.getValue()];
+            if(low!=null)
+                lower[i]=higher[low.getValue()];
             if(higher[i])
-                res++;
+                count++;
             map.put(A[i],i);
         }
-        
-        return res;
+        return count;
     }
 }
